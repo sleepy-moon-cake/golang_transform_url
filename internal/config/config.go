c4bd73057a46326824c2b5acc485bbf9fb125019
Moon Cake <mooncake@Moons-MacBook-Pro.local>
2026-06-14 12:19:21 +0300

    iter25: support file config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,21 +1,24 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"strconv"
 )
 
 type Config struct {
-	ServerAddress   string
-	BaseURLAddress  string
-	LoggerLevel     string
-	FileStoragePath string
-	DatabaseDSN     string
-	AuditFile       string
-	AuditURL        string
-	Secure          bool
+	ServerAddress   string `json:"server_address"`
+	BaseURLAddress  string `json:"base_url"`
+	LoggerLevel     string `json:"logger_level"`
+	FileStoragePath string `json:"file_storage_path"`
+	DatabaseDSN     string `json:"database_dsn"`
+	AuditFile       string `json:"audit_file"`
+	AuditURL        string `json:"audit_url"`
+	Secure          bool   `json:"enable_https"`
+	ConfigFilePath  string `json:"-"`
 }
 
 func GetConfig() *Config {
@@ -29,6 +32,7 @@ func GetConfig() *Config {
 	flag.StringVar(&config.AuditFile, "audit-file", "", "path to audit log file")
 	flag.StringVar(&config.AuditURL, "audit-url", "", "remote audit server url")
 	flag.BoolVar(&config.Secure, "s", false, "use https")
+	flag.StringVar(&config.ConfigFilePath, "c", "", "configuration file path")
 
 	flag.Parse()
 
@@ -68,6 +72,19 @@ func GetConfig() *Config {
 		}
 	}
 
+	if configPath, ok := os.LookupEnv("CONFIG"); ok {
+		config.ConfigFilePath = configPath
+	}
+
+	if config.ConfigFilePath != "" {
+		fileConfigs, err := getFileConfigs(config.ConfigFilePath)
+		if err != nil {
+			slog.Error("FileConfig", "error", err)
+		} else {
+			mergeConfigs(&config, fileConfigs)
+		}
+	}
+
 	slog.Info("Configurations:::",
 		slog.String("Server address", config.ServerAddress),
 		slog.String("Base short URL", config.BaseURLAddress),
@@ -75,4 +92,49 @@ func GetConfig() *Config {
 	)
 
 	return &config
+}
+
+func getFileConfigs(path string) (*Config, error) {
+	f, err := os.Open(path)
+
+	if err != nil {
+		return nil, fmt.Errorf("fail to open file: %w", err)
+	}
+	defer f.Close()
+
+	var fileConfigs Config
+
+	if err := json.NewDecoder(f).Decode(&fileConfigs); err != nil {
+		return nil, fmt.Errorf("fail to decode json config: %w", err)
+	}
+
+	return &fileConfigs, nil
+}
+
+// перенести в генератор
+func mergeConfigs(scfg *Config, fcfg *Config) {
+	if scfg.ServerAddress == "" {
+		scfg.ServerAddress = fcfg.ServerAddress
+	}
+	if scfg.BaseURLAddress == "" {
+		scfg.BaseURLAddress = fcfg.BaseURLAddress
+	}
+	if scfg.LoggerLevel == "" {
+		scfg.LoggerLevel = fcfg.LoggerLevel
+	}
+	if scfg.FileStoragePath == "" {
+		scfg.FileStoragePath = fcfg.FileStoragePath
+	}
+	if scfg.DatabaseDSN == "" {
+		scfg.DatabaseDSN = fcfg.DatabaseDSN
+	}
+	if scfg.AuditFile == "" {
+		scfg.AuditFile = fcfg.AuditFile
+	}
+	if scfg.AuditURL == "" {
+		scfg.AuditURL = fcfg.AuditURL
+	}
+	if !scfg.Secure {
+		scfg.Secure = fcfg.Secure
+	}
 }
